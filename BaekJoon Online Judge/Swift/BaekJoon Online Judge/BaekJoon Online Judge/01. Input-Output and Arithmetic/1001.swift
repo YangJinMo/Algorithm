7f9baf9a3efa03a80f40dd41ac4ec89044ab590c
Jmy <didwlsah9@gmail.com>
2021-07-03 18:51:41 +0900

    Update 1001

--- BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/01. Input-Output and Arithmetic/1001.swift
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/01. Input-Output and Arithmetic/1001.swift
@@ -26,9 +26,12 @@ class Problem_1001 {
   func solution_3() {
     print(readLine()!.split{$0==" "}.map{Int($0)!}.reduce(0){-$0-$1})
   }
+  func solution_4() {
+    let l=readLine()!.split{$0==" "}.map{Int($0)!};print(l[0]-l[1])
+  }
   /// https://www.acmicpc.net/short/status/1001/74/1
   /// 1등
   func solution_zzimss() {
-    let l=readLine()!.split{$0==" "}.map{Int($0)!};print(l[0]-l[1])
+    let l=readLine()!.split{$0==" "};print(Int(l[0])!-Int(l[1])!)
   }
 }
