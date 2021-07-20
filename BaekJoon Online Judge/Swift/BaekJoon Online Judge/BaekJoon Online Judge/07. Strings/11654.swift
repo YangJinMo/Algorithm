8111307bad0c951d86cb85391040f0fcdcddbaea
Jmy <didwlsah9@gmail.com>
2021-07-20 22:56:50 +0900

    Add 11654

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/07. Strings/11654.swift
@@ -0,0 +1,25 @@
+//
+//  11654.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/07/20.
+//
+
+/**
+ - 제목:
+ - 문제:
+ - 입력:
+ - 출력:
+ - 원문: https://www.acmicpc.net/problem/11654
+ */
+
+class Problem_11654 {
+  func solution_1() {
+    
+  }
+  /// https://www.acmicpc.net/short/status/11654/74/1
+  /// 1등
+  func solution_zzimss() {
+    
+  }
+}
