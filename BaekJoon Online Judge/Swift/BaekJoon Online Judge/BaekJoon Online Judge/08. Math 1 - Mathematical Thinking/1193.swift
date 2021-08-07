1d0b324d27099eca14cffae243b8eb77f884eec0
Jmy <didwlsah9@gmail.com>
2021-08-07 17:00:57 +0900

    Add 1193

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/1193.swift
@@ -0,0 +1,26 @@
+//
+//  1193.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/08/07.
+//
+
+/**
+ - 제목: 분수찾기
+ - 문제: 이와 같이 나열된 분수들을 1/1 -> 1/2 -> 2/1 -> 3/1 -> 2/2 -> … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+       X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+ - 입력: 첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
+ - 출력: 첫째 줄에 분수를 출력한다.
+ - 원문: https://www.acmicpc.net/problem/1193
+ */
+
+class Problem_1193 {
+  func solution_1() {
+    
+  }
+  /// https://www.acmicpc.net/short/status/1193/74/1
+  /// 등
+  func solution_zzimss() {
+    
+  }
+}
