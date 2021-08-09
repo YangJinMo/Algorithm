9df65bb3e82310981eeebe01b3800e8aac0a80ec
Jmy <didwlsah9@gmail.com>
2021-08-09 14:26:44 +0900

    Add 2869

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/2869.swift
@@ -0,0 +1,36 @@
+//
+//  2869.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/08/09.
+//
+
+/**
+ - 제목: 달팽이는 올라가고 싶다
+ - 문제: 땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+       달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
+       달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+ - 입력: 첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B `<` A ≤ V ≤ 1,000,000,000)
+ - 출력: 첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.
+ - 원문: https://www.acmicpc.net/problem/2869
+ */
+
+class Problem_2869 {
+  func solution_1() {
+    
+  }
+  func solution_2() {
+    
+  }
+  func solution_3() {
+    
+  }
+  func solution_4() {
+    
+  }
+  /// https://www.acmicpc.net/short/status/2869/74/1
+  /// 등
+  func solution_zzimss() {
+    
+  }
+}
