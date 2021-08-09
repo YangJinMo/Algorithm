575d3f32d6cac83f69ffdd8709ce060a2f009f36
Jmy <didwlsah9@gmail.com>
2021-08-09 15:47:40 +0900

    Update 2869

--- BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/2869.swift
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/2869.swift
@@ -17,20 +17,62 @@
 
 class Problem_2869 {
   func solution_1() {
+    /// 시간 초과
+    let line = readLine()!.split{$0==" "}.map{Int($0)!}
+    let a = line[0]
+    let b = line[1]
+    let v = line[2]
+    var r = 0
+    var i = 0
     
+    while v >= r {
+      i += 1
+      r += a
+      if v <= r {
+        print(i)
+        break
+      }
+      r -= b
+    }
   }
   func solution_2() {
+    /// 시간 초과
+    let line = readLine()!.split{$0==" "}.map{Int($0)!}
+    let a = line[0]
+    let b = line[1]
+    let v = line[2]
+    var i = 0
     
+    while true {
+      i += 1
+      if a*i-b*(i-1) >= v {
+        print(i)
+        break
+      }
+    }
   }
   func solution_3() {
-    
+    let line = readLine()!.split{$0==" "}.map{Int($0)!}
+    let a = line[0]
+    let b = line[1]
+    let v = line[2]
+    // a*i-b*(i-1) >= v
+    // a*i-b*i+b >= v
+    // (a-b)*i+b >= v
+    // (a-b)*i >= v-b
+    let x = (v-b)/(a-b)
+    let y = (v-b)%(a-b)
+    print(x+y)
   }
   func solution_4() {
-    
+    let l=readLine()!.split{$0==" "}.map{Int($0)!},a=l[2]-l[1],b=l[0]-l[1];print((a%b>0 ?1:0)+a/b)
+  }
+  func solution_5() {
+    let l=readLine()!.split{$0==" "}.map{Int($0)!},a=l[2]-l[1],b=l[0]-l[1];print(a%b>0 ?a/b+1:a/b)
   }
   /// https://www.acmicpc.net/short/status/2869/74/1
-  /// 등
+  /// 2등 - 같은 코드
   func solution_zzimss() {
-    
+    let l=readLine()!.split{$0==" "}.map{Int($0)!};print(1+(l[2]-l[1]-1)/(l[0]-l[1]))
   }
 }
--- BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/main.swift
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/main.swift
@@ -5,5 +5,5 @@
 //  Created by Jmy on 2021/06/07.
 //
 
-let problem = Problem_1193()
+let problem = Problem_2869()
 problem.solution_zzimss()
