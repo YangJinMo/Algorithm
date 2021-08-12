47bd25a6894168b067b0ea65188dae2003da955f
Jmy <didwlsah9@gmail.com>
2021-08-12 12:52:32 +0900

    Add 2839

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/2839.swift
@@ -0,0 +1,31 @@
+//
+//  2839.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/08/12.
+//
+
+/**
+ - 제목: 설탕 배달
+ - 원문: https://www.acmicpc.net/problem/2839
+ */
+
+class Problem_2839 {
+  func solution_1() {
+    
+  }
+  func solution_2() {
+    
+  }
+  func solution_3() {
+    
+  }
+  func solution_4() {
+    
+  }
+  /// https://www.acmicpc.net/short/status/2839/74/1
+  /// 등
+  func solution_zzimss() {
+    
+  }
+}
