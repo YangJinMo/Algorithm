961f6c35b89ccad0c88536f0e48c0c8d3268cd9a
Jmy <didwlsah9@gmail.com>
2021-08-14 01:35:10 +0900

    Add 10757

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/10757.swift
@@ -0,0 +1,40 @@
+//
+//  10757.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/08/13.
+//
+
+/**
+ - 제목: 큰 수 A+B
+ - 문제: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ - 입력: 첫째 줄에 A와 B가 주어진다. (0 `<` A,B `<` 10^10000)
+ - 출력: 첫째 줄에 A+B를 출력한다.
+ - 원문: https://www.acmicpc.net/problem/10757
+ */
+
+class Problem_10757 {
+  func solution_1() {
+    
+  }
+  func solution_2() {
+    
+  }
+  func solution_3() {
+    
+  }
+  func solution_4() {
+    
+  }
+  func solution_5() {
+    
+  }
+  func solution_6() {
+    
+  }
+  /// https://www.acmicpc.net/short/status/10757/74/1
+  /// 등
+  func solution_zzimss() {
+    
+  }
+}
