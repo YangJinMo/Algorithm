6fa3e7671d1ebe1dd4e9757a3098610e174c4ac7
Jmy <didwlsah9@gmail.com>
2021-08-16 22:52:15 +0900

    Add 1011

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/1011.swift
@@ -0,0 +1,40 @@
+//
+//  1011.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/08/16.
+//
+
+/**
+ - 제목: Fly me to the Alpha Centauri
+ - 문제: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ - 입력: 입력의 첫 줄에는 테스트케이스의 개수 T가 주어진다. 각각의 테스트 케이스에 대해 현재 위치 x 와 목표 위치 y 가 정수로 주어지며, x는 항상 y보다 작은 값을 갖는다. (0 ≤ x `<` y `<` 231)
+ - 출력: 각 테스트 케이스에 대해 x지점으로부터 y지점까지 정확히 도달하는데 필요한 최소한의 공간이동 장치 작동 횟수를 출력한다.
+ - 원문: https://www.acmicpc.net/problem/1011
+ */
+
+class Problem_1011 {
+  func solution_1() {
+    
+  }
+  func solution_2() {
+    
+  }
+  func solution_3() {
+    
+  }
+  func solution_4() {
+    
+  }
+  func solution_5() {
+    
+  }
+  func solution_6() {
+    
+  }
+  /// https://www.acmicpc.net/short/status/1011/74/1
+  /// 등
+  func solution_zzimss() {
+    
+  }
+}
