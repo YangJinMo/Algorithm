e3f5ff4a2e5bd6dd48ddf3e84c54c868aa406823
Jmy <didwlsah9@gmail.com>
2021-08-22 23:46:05 +0900

    Update 1011

--- BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/1011.swift
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/08. Math 1 - Mathematical Thinking/1011.swift
@@ -13,28 +13,168 @@
  - 원문: https://www.acmicpc.net/problem/1011
  */
 
+// 1 1 1
+// 2 2 11
+// 3 3 111
+// 4 3 121
+// 5 4 1211
+// 6 4 1221
+// 7 5 12121
+// 8 5 12221
+// 9 5 12321
+//10 6 123211, 122221
+
 class Problem_1011 {
-  func solution_1() {
-    
-  }
-  func solution_2() {
-    
-  }
-  func solution_3() {
-    
-  }
-  func solution_4() {
-    
-  }
-  func solution_5() {
-    
-  }
-  func solution_6() {
-    
-  }
-  /// https://www.acmicpc.net/short/status/1011/74/1
-  /// 등
-  func solution_zzimss() {
-    
-  }
+    func solution_1_1() {
+        let T = Int(readLine()!)!
+        
+        for _ in 1...T {
+            let arrInt = readLine()!.split(separator: " ").map { Int($0)! }
+            var distance = arrInt[1] - arrInt[0] // 이동 거리
+            var k = 1 // 광년
+            var i = 0 // 이동 장치 작동 횟수의 최솟값
+            
+            while distance > 0 {
+                distance -= k
+                i += 1
+                
+                if distance > 0 {
+                    distance -= k
+                    i += 1
+                    
+                    if distance == 1 {
+                        i += 1
+                        break
+                    } else {
+                        k += 1
+                    }
+                }
+            }
+            print(i, k)
+        }
+    }
+    func solution_1_2() {
+        for _ in 1...Int(readLine()!)!{var l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!,k=1,i=0;while d>0{d-=k;i+=1;if d>0{d-=k;i+=1;if d==1{i+=1;break}else{k+=1}}};print(i)}
+    }
+    func solution_1_3() {
+        for _ in 1...Int(readLine()!)!{var d=readLine()!.split{$0==" "}.map{Int($0)!}.reduce(0){$1-$0},k=1,i=0;while d>0{d-=k;i+=1;if d>0{d-=k;i+=1;if d==1{i+=1;break}else{k+=1}}};print(i)}
+    }
+    func solution_1_4() {
+        for _ in 1...Int(readLine()!)! {
+            let arrInt = readLine()!.split(separator: " ").map { Int($0)! }
+            var distance = arrInt[1] - arrInt[0] // 이동 거리
+            var k = 1 // 광년
+            var i = 0 // 이동 장치 작동 횟수의 최솟값
+            
+            while distance > 0 {
+                distance -= k
+                i += 1
+                
+                if distance > 0 {
+                    distance -= k
+                    i += 1
+                    
+                    if distance == 1 {
+                        i += 1
+                        break
+                    } else {
+                        k += 1
+                    }
+                }
+            }
+            print(i, k)
+        }
+    }
+    /// 원리가 뭘까?
+    func solution_2_1() {
+        for _ in 1...Int(readLine()!)! {
+            var end: Int = readLine()!.split{$0==" "}.map{Int($0)!}.reduce(0){-$0-$1}*(-1)*4,k=1,i=1
+            
+            while k < end {
+                i += 1
+                k = (i + 1) * (i + 1)
+            }
+            print(i)
+        }
+    }
+    func solution_2_2() {
+        for _ in 1...Int(readLine()!)!{var l=readLine()!.split{$0==" "},d=(Int(l[1])!-Int(l[0])!)*4,k=1,i=1;while k<d{i+=1;k=i*i};print(i-1)}
+    }
+    func solution_3() {
+        for _ in 1...Int(readLine()!)! {
+            let l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!
+            
+            for i in 1... {
+                let v = i%2 == 0 ? ((i/2)*(i/2+1)) : ((i/2)*(i/2+1))/2 + ((i/2+1)*(i/2+2))/2
+                if d <= v {
+                    print(i)
+                    break
+                }
+            }
+        }
+    }
+    func solution_4() {
+        for _ in 1...Int(readLine()!)! {
+            var l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!,k=1,i=1
+            
+            while true {
+                if d <= k*k {
+                    if k*k-k < d {
+                        i = 2*k-1
+                    } else {
+                        i = 2*(k-1)
+                    }
+                    break
+                }
+                k += 1
+            }
+            print(i)
+        }
+    }
+    func solution_5_1() {
+        for _ in 1...Int(readLine()!)! {
+            let l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!
+            var k = 1
+            var i = 1
+            var e = 1
+            
+            while d > e {
+                e += k
+                i += 1
+                
+                if i % 2 == 0 {
+                    k += 1
+                }
+            }
+            print(i)
+        }
+    }
+    func solution_5_2() {
+        for _ in 1...Int(readLine()!)! {
+            var l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!
+            var k = 1
+            var i = 1
+            
+            while d > 1 {
+                d -= k
+                i += 1
+                
+                if i % 2 == 0 {
+                    k += 1
+                }
+            }
+            print(i)
+        }
+    }
+    func solution_5_3() {
+        for _ in 1...Int(readLine()!)!{var l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!,k=1,i=1,e=1;while d>e{e+=k;i+=1;if i%2==0{k+=1}};print(i)}
+    }
+    func solution_5_4() {
+        for _ in 1...Int(readLine()!)!{var l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!,k=1,i=1;while d>1{d-=k;i+=1;if i%2==0{k+=1}};print(i)}
+    }
+    /// https://www.acmicpc.net/short/status/1011/74/1
+    /// 1등
+    func solution_zzimss() {
+        for _ in 1...Int(readLine()!)!{var l=readLine()!.split{$0==" "},d=Int(l[1])!-Int(l[0])!,k=1,i=1;while d>1{d-=k;i+=1;if i%2==0{k+=1}};print(i)}
+    }
 }
--- BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/main.swift
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/main.swift
@@ -5,5 +5,5 @@
 //  Created by Jmy on 2021/06/07.
 //
 
-let problem = Problem_10757()
-problem.solution_zzimss()
+let problem = Problem_1011()
+problem.solution_6_2()
