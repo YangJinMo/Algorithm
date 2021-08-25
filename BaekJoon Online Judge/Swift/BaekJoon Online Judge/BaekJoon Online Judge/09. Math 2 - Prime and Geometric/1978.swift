e5e4e3ded5dccfe357e7cb3481f21da79b384451
Jmy <didwlsah9@gmail.com>
2021-08-25 19:46:10 +0900

    Add 1978

---
+++ BaekJoon Online Judge/Swift/BaekJoon Online Judge/BaekJoon Online Judge/09. Math 2 - Prime and Geometric/1978.swift
@@ -0,0 +1,25 @@
+//
+//  1978.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/08/25.
+//
+
+/**
+ - 제목: 소수 찾기
+ - 문제: 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+ - 입력: 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+ - 출력: 주어진 수들 중 소수의 개수를 출력한다.
+ - 원문: https://www.acmicpc.net/problem/1978
+ */
+
+class Problem_1978 {
+    func solution_1() {
+        
+    }
+    /// https://www.acmicpc.net/short/status/1978/74/1
+    /// 등
+    func solution_zzimss() {
+        
+    }
+}
