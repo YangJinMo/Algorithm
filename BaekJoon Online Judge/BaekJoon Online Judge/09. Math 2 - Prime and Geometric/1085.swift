a9f535bba31bfd26a5d87d822ca3615e2000d197
Jmy <didwlsah9@gmail.com>
2021-09-25 11:52:10 +0900

    Update 1085

--- BaekJoon Online Judge/BaekJoon Online Judge/09. Math 2 - Prime and Geometric/1085.swift
+++ BaekJoon Online Judge/BaekJoon Online Judge/09. Math 2 - Prime and Geometric/1085.swift
@@ -16,12 +16,21 @@
 
 class Problem_1085 {
     func solution_1() {
-        
+        var arrInt: [Int] = readLine()!.split(separator: " ").compactMap { Int($0) }
+        let (x, y, w, h) = (arrInt[0], arrInt[1], arrInt[2], arrInt[3])
+        arrInt.append(w-x)
+        arrInt.append(h-y)
+        print(arrInt.min()!)
     }
 
     /// https://www.acmicpc.net/short/status/1085/74/1
-    /// 등
+    /// 2등
     func solution_short_1() {
-        
+        let l=readLine()!.split{$0==" "}.map{Int($0)!}
+        print(min(l[0],l[1],l[2]-l[0],l[3]-l[1]))
+    }
+    func solution_short_2() {
+        let l=readLine()!.split{$0==" "}.map{Int($0)!},x=l[0],y=l[1]
+        print(min(x,y,l[2]-x,l[3]-y))
     }
 }
--- BaekJoon Online Judge/BaekJoon Online Judge/main.swift
+++ BaekJoon Online Judge/BaekJoon Online Judge/main.swift
@@ -5,5 +5,5 @@
 //  Created by Jmy on 2021/06/07.
 //
 
-let problem = Problem_9020()
-problem.solution_short_1()
+let problem = Problem_1085()
+problem.solution_1()
