7677cfc65c8298a167261c92b8ff3c1966541f6d
Jmy <didwlsah9@gmail.com>
2021-09-29 14:35:52 +0900

    Add 4153

---
+++ BaekJoon Online Judge/BaekJoon Online Judge/09. Math 2 - Prime and Geometric/4153.swift
@@ -0,0 +1,27 @@
+//
+//  4153.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/09/29.
+//
+
+/**
+ - 제목: 직각삼각형
+ - 문제: 과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+ - 입력: 입력은 여러개의 테스트케이스로 주어지며 마지막줄에는 0 0 0이 입력된다.
+       각 테스트케이스는 모두 30,000보다 작은 양의 정수로 주어지며, 각 입력은 변의 길이를 의미한다.
+ - 출력: 각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
+ - 원문: https://www.acmicpc.net/problem/4153
+ */
+
+class Problem_4153 {
+    func solution_1() {
+        
+    }
+
+    /// https://www.acmicpc.net/short/status/4153/74/1
+    /// 등
+    func solution_short_1() {
+        
+    }
+}
