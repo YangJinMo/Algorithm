c2f90f82e84d3b87e1ce06fb1de235260bbc40ec
Jmy <didwlsah9@gmail.com>
2021-10-03 19:52:33 +0900

    Add 10872

---
+++ BaekJoon Online Judge/BaekJoon Online Judge/10. Recursion/10872.swift
@@ -0,0 +1,26 @@
+//
+//  10872.swift
+//  BaekJoon Online Judge
+//
+//  Created by Jmy on 2021/10/03.
+//
+
+/**
+ - 제목: 팩토리얼
+ - 문제: 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+ - 입력: 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+ - 출력: 첫째 줄에 N!을 출력한다.
+ - 원문: https://www.acmicpc.net/problem/10872
+ */
+
+class Problem_10872 {
+    func solution_1() {
+        
+    }
+
+    /// https://www.acmicpc.net/short/status/10872/74/1
+    /// 등
+    func solution_short_1() {
+        
+    }
+}
