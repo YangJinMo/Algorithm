6c8d23bddf3493c8cd2e99dcd5cc933635db0a54
Jmy <didwlsah9@gmail.com>
2021-10-09 16:00:18 +0900

    Update 2447

--- BaekJoon Online Judge/BaekJoon Online Judge/10. Recursion/2447.swift
+++ BaekJoon Online Judge/BaekJoon Online Judge/10. Recursion/2447.swift
@@ -15,14 +15,75 @@
  - 원문: https://www.acmicpc.net/problem/2447
  */
 
+/// 분할 정복 알고리즘(Divide and conquer algorithm)
+/// 그대로 해결할 수 없는 문제를 작은 문제로 분할하여 문제를 해결하는 방법이나 알고리즘이다.
+/// 빠른 정렬이나 합병 정렬로 대표되는 정렬 알고리즘 문제와 고속 푸리에 변환(FFT) 문제가 대표적이다.
+
 class Problem_2447 {
     func solution_1() {
+        let N = Int(readLine()!)!
+        var res = ""
+
+        func printStar(_ row: Int, _ col: Int, _ N: Int) {
+            if row % 3 == 1 && col % 3 == 1 {
+                res += " "
+            } else if N == 1 {
+                res += "*"
+            } else {
+                printStar(row/3, col/3, N/3)
+            }
+        }
+
+        for row in 0 ..< N {
+            for col in 0 ..< N {
+                printStar(row, col, N)
+            }
+            res += "\n"
+        }
+        print(res)
+    }
+    
+    func solution_2() {
+        func f(num: Int, pattern: [String]) {
+            if num == 1 {
+                pattern.forEach { print($0) }
+                return
+            }
+            
+            let row1: [String] = pattern.map { $0 + $0 + $0 }
+            let row2: [String] = pattern.map { $0 + String(repeating: " ", count: $0.count) + $0 }
+            let newPattern: [String] = row1 + row2 + row1
+            
+            f(num: num/3, pattern: newPattern)
+        }
         
+        let N = Int(readLine()!)!
+        f(num: N, pattern: ["*"])
+    }
+    
+    func solution_3() {
+        let N=Int(readLine()!)!
+        var r=Array(repeating:Array(repeating:"*",count:N),count:N)
+        func f(_ r: inout [[String]],_ a:Int,_ b:Int,_ c:Int){
+            let t=c/3
+            if t<1{return}
+            for x in a+t..<a+t*2{for y in b+t..<b+t*2{r[x][y]=" "}}
+            for x in 0...2{for y in 0...2{f(&r,a+t*x,b+t*y,t)}}
+        }
+        f(&r,0,0,N)
+        r.forEach{print($0.joined())}
     }
     
     /// https://www.acmicpc.net/short/status/2447/74/1
-    /// 등
+    /// 1등
     func solution_short_1() {
-        
+        var N=Int(readLine()!)!,p=""
+        func f(_ r:Int,_ c:Int,_ N:Int){if r%3==1&&c%3==1{p+=" "}else if N==1{p+="*"}else{f(r/3,c/3,N/3)}}
+        for r in 0..<N{for c in 0..<N{f(r,c,N)};p+="\n"}
+        print(p)
+    }
+    
+    func solution_short_2() {
+        func f(n:Int,p:[String]){if n==1{p.forEach{print($0)};return};let a=p.map{$0+$0+$0};f(n:n/3,p:a+p.map{$0+String(repeating:" ",count:$0.count)+$0}+a)};f(n:Int(readLine()!)!,p:["*"])
     }
 }
--- BaekJoon Online Judge/BaekJoon Online Judge/main.swift
+++ BaekJoon Online Judge/BaekJoon Online Judge/main.swift
@@ -5,5 +5,5 @@
 //  Created by Jmy on 2021/06/07.
 //
 
-let problem = Problem_10870()
+let problem = Problem_2447()
 problem.solution_1()
